824910ae3f1d70a08124f5a7740eb2157803c349
AndreasAugustin <dev@andreas-augustin.org>
2021-03-19 23:25:18 +0100

    fix(chore): fix docker mounts and env

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const HttpsProxyAgent = require("https-proxy-agent");
 const sdk = require("aws-sdk");
+const url = require("url");
 
 const handler = async (event) => {
-  const proxy = "http://what.ever.com:80";
-  const url = new URL(proxy);
-  const proxyAgent = new HttpsProxyAgent(url);
+  const proxy = 'http://what.ever.com:8080';
+  const urlProxy = new URL(proxy);
+
+  const urlproxy = url.parse(proxy);
+
+  const proxyAgent = new HttpsProxyAgent(urlProxy);
 
   sdk.config.httpOptions.agent = proxyAgent;
   const s3 = new sdk.S3();
   const res = await s3.listBuckets().promise();
 
-  console.log("event", event);
+  //Fconsole.log("event", event);
   return {
     foo: "bar",
   };
